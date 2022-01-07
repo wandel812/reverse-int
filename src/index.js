da1b9c4555fc9e5af90cf7daf42d2bcf4fe67f4b
= <=>
2022-01-07 16:49:07 +0400

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) { 
+    const strRepr = n.toString();
+    const reversedStrRepr = strRepr.split("").reverse().join("");
+    return Number.parseInt(reversedStrRepr);
 }
